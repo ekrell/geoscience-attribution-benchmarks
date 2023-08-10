b8ff7a0574c8dfc3349fc5345ed6e038705d73bd
Evan Krell <ekrell@islander.tamucc.edu>
2023-08-10 15:54:57 -0500

    Removed unused import

--- models/train_nn.py
+++ models/train_nn.py
@@ -13,7 +13,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from optparse import OptionParser
 import matplotlib.pyplot as plt
