f88093a7004d94d3de70954adab8845ed37319b8
Evan Krell <ekrell@islander.tamucc.edu>
2023-09-28 17:04:49 -0500

    Added tool to generated covariance matrices along spectrum of no correlation to lots

---
+++ utils/generate_covariances.py
@@ -0,0 +1,29 @@
+# This script generates custom covariance matrices
+
+import numpy as np
+
+# Where to save covariance matrices
+out_dir = "out/cov_exp/"
+out_file_fmt = out_dir + "/cov_{}.npz"
+
+# Number of rows & cols of synthetic data
+rows = 18
+cols = 36
+
+# Rows, cols of square covariance matrix
+n = rows * cols
+
+weights = np.linspace(0.1, 1, 10)
+
+ones = np.ones(n)
+identity = np.identity(n)
+
+mask = np.ones((rows, cols))
+
+for i, w in enumerate(weights):
+  cov = w * ones + (1.0 - w) * identity
+
+  print(w, (1-w), i)
+
+  np.savez(out_file_fmt.format(i), covariance=cov, mask=mask)
+
