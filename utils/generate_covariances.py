9222efb08141f532852180e0675d83f56aa001bf
Evan Krell <ekrell@islander.tamucc.edu>
2023-10-10 14:50:43 -0500

    Plotting cov matrices

--- utils/generate_covariances.py
+++ utils/generate_covariances.py
@@ -1,14 +1,16 @@
 # This script generates custom covariance matrices
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Where to save covariance matrices
 out_dir = "out/cov_exp/"
 out_file_fmt = out_dir + "/cov_{}.npz"
+out_plot_fmt = out_dir + "/cov_{}.pdf"
 
 # Number of rows & cols of synthetic data
-rows = 18
-cols = 36
+rows = 20 #18
+cols = 23 #36
 
 # Rows, cols of square covariance matrix
 n = rows * cols
@@ -22,4 +24,8 @@ mask = np.ones((rows, cols))
 
 for i, w in enumerate(weights):
   cov = w * ones + (1.0 - w) * identity
+
+  plt.imshow(cov, cmap="gray_r", vmax=1, vmin=0)
+  plt.savefig(out_plot_fmt.format(i))
+
   np.savez(out_file_fmt.format(i), covariance=cov, mask=mask)
