51e5af5fa9e69660cbd6fd9936076fd4b0c01352
Evan Krell <ekrell@islander.tamucc.edu>
2024-02-13 11:15:02 -0600

    Additional fixes to wget download

--- benchmarks/sstanom/create_sstanom_benchmark.sh
+++ benchmarks/sstanom/create_sstanom_benchmark.sh
@@ -8,16 +8,16 @@ mkdir -p ${sstanom_dir}/data/
 mkdir -p ${sstanom_dir}/out/
 
 # Download SST data
-if test -f "${covariance_file}"; then
-  echo "Found sst data file '${covariance_file}'. Skipping download..."
+if test -f "${sst_data_file}"; then
+  echo "Found sst data file '${sst_data_file}'. Skipping download..."
 else
-  wget https://downloads.psl.noaa.gov//Datasets/COBE2/sst.mon.mean.nc -o ${sst_data_file}
+  wget https://downloads.psl.noaa.gov//Datasets/COBE2/sst.mon.mean.nc -P ${sstanom_dir}/data/
 fi
 
 # Calculate covariance from real SST data
 python ${sstanom_dir}/get_sst.py \
   -n ${sst_data_file} \
   -c ${covariance_file} 
-
+exit 0
 # The rest is handled by a generic pipeline
 bash pipelines/basic_covariance_benchmark_pipeline.bash ${config_file}
