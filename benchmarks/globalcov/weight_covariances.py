ffe16ebf44918e12af59bd6bb1f2e3cd868097c8
Evan Krell <ekrell@islander.tamucc.edu>
2024-02-27 11:54:51 -0600

    Implemented a simple scheme to incrementally increase the (postive) correlation strenght

--- benchmarks/globalcov/weight_covariances.py
+++ benchmarks/globalcov/weight_covariances.py
@@ -52,35 +52,18 @@ cor_orig = cov_orig
 vals_orig = cor_orig[np.triu_indices_from(cor_orig)]
 
 # Init storage for modified covariance values
-cor_vals = np.zeros((vals_orig.shape[0], len(weights)))
+cor_vals = np.zeros((vals_orig.shape[0] - len(cor_orig), len(weights)))
+
+# Create fully-correlated base matrix
+fully_corr = np.ones(cor_orig.shape[0])
 
 # Increase covariance strength
 for i, wi in enumerate(weights):
   # Make a copy
   cor = cor_orig.copy()
 
-  n = cor.shape[0]
-  ones = np.ones(n)
-  ident = np.identity(n)
-  #c = wi * ones + (1.0 - wi) * ident
-  c = 1 * ones + (1.0 - 1)* ident
-
-  cor = (1.0 - wi) * cor + (wi) * c
-  # Separately deal with pos and neg correlations
-  #cor = cor + cor * wi * (cor >= 0)
-  #cor = cor - cor * -wi * (cor < 0)
-
-  # Remove outliers (and ensure within (-1, 1))
-  #poss = cor[cor > 0]
-  #top = np.min([np.percentile(poss, 97, axis=0), 0.99])
-  #negs = -cor[cor < 0]
-  #bot = np.max([np.percentile(negs, 97, axis=0), -0.99])
-  #cor[cor > top] = top
-  #cor[cor < -bot] = -bot
-
-  #cor[cor > 1] = 1
-
-  # Put ones in the diag
+  # New correlation matrix is weighted sum of original and fully
+  cor = (1.0 - wi) * cor + (wi) * fully_corr
 
   # Plot matrix
   print("Generated new cov matrix: {}".format(out_file_fmt.format(i)))
@@ -90,20 +73,26 @@ for i, wi in enumerate(weights):
 
   # Store the correlation values
   cor = cov2cor(cor)
-  cor_vals[:, i] = cor[np.triu_indices_from(cor)].flatten()
+  cor_vals[:, i] = cor[np.triu_indices_from(cor, k=1)].flatten()
 
-
-
-# Compare correlations
-fig, ax = plt.subplots()
-for i in range(0,len(weights)):
-    ax.violinplot(dataset=cor_vals[:, i], positions=[i])
+# Compare correlation distributions
 weight_strs = [""] + ["{0:.2f}".format(wi) for wi in weights]
-ax.set_xticklabels(weight_strs)
-ax.set_ylim(-1.1, 1.1)
-ax.axhline(y=-1, color='r', linestyle='--')
-ax.axhline(y=1, color='r', linestyle='--')
-ax.set_xlabel("Added correlation weight")
-ax.set_ylabel("Correlation coefficient")
-ax.set_title("Comparison of correlation matrix values")
+fig, ax = plt.subplots(2)
+for i in range(0,len(weights)):
+    ax[0].violinplot(dataset=cor_vals[:, i], positions=[i])
+    ax[1].violinplot(dataset=np.abs(cor_vals[:, i]), positions=[i])
+# Set y limits
+ax[0].set_ylim(-1., 1.)
+ax[1].set_ylim(0, 1.)
+# Set x labels
+ax[0].set_xticklabels([]) 
+ax[1].set_xticklabels(weight_strs)
+ax[1].set_xlabel("correlation weight")
+# Set y labels
+ax[0].set_ylabel("Correlation coefficient")
+ax[1].set_ylabel("Correlation coefficient")
+
+ax[0].set_title("Correlation matrix values")
+ax[1].set_title("Correlation matrix absolute values")
+
 plt.savefig(outplot_file)
